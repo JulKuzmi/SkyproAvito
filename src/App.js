0babf1ba82ca80ba3a245c931363ec7b6959a86b
JulKuzmi <enchantress000@mail.ru>
2023-11-22 21:45:39 +0300

    css/formatters

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
- 
- 
-
+import * as S from './styles/App.styles';
+import { AppRoutes } from './routes';
 function App() {
-   
+    return (
+        <S.Wrapper>
+            <S.Container>
+                <S.StyLeGlobal />
+                <AppRoutes />
+            </S.Container>
+        </S.Wrapper>
+    );
 }
 
 export default App;
