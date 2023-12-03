4340a4cff54b250325faa54d30262d248f4a66b8
JulKuzmi <enchantress000@mail.ru>
2023-12-03 11:29:13 +0300

    not found 404

---
+++ src/pages/notFound/notFound.jsx
@@ -0,0 +1,19 @@
+import * as S from './notFound.style';
+import { Link } from 'react-router-dom';
+
+export const NotFound = () => {
+    return (
+        <S.notFound>
+            <S.notFoundCode>404</S.notFoundCode>
+            <S.notFoundTextWrap>
+                <S.notFoundText>Страница не найдена</S.notFoundText>
+            </S.notFoundTextWrap>
+            <S.notFoundDescription>
+                Возможно, она была удалена или перенесена на другой адрес
+            </S.notFoundDescription>
+            <Link to="/">
+                <S.notFoundButton>Вернуться на главную</S.notFoundButton>
+            </Link>
+        </S.notFound>
+    );
+};
